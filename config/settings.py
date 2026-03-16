e5f290e5cf6ec4626f387d9539fddd0897fa5b27
Dark-Angel-One <jegorsavinoff@yandex.ru>
2026-03-16 21:26:20 +0300

    Добавил sslip.io в разрешенные хосты

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os
-import sys  # ТИМЛИД: Добавили sys для проверки запущенных команд
+import sys
 from django.core.exceptions import ImproperlyConfigured
 from dotenv import load_dotenv
 
@@ -14,10 +14,16 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 if not SECRET_KEY:
     raise ImproperlyConfigured("SECRET_KEY must be set in .env")
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else []
+# ТИМЛИД: Добавили новый домен sslip.io для работы HTTPS
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else [
+    '144.31.62.182', 
+    '144-31-62-182.sslip.io', 
+    'localhost', 
+    '127.0.0.1'
+]
 
 INSTALLED_APPS = [
-    'todo_sql', # ТИМЛИД: Выносим наше приложение наверх, чтобы его шаблоны (и админки) были главными!
+    'todo_sql',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -28,7 +34,6 @@ INSTALLED_APPS = [
     'django_filters',
 ]
 
-# ТИМЛИД: Подключаем наш кастомный бэкенд для Email
 AUTHENTICATION_BACKENDS = [
     'todo_sql.backends.EmailOrUsernameModelBackend',
     'django.contrib.auth.backends.ModelBackend',
@@ -59,7 +64,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'], # ТИМЛИД: Указываем папку для переопределения админки
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -102,7 +107,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Настройки Email
 if os.getenv('EMAIL_HOST_USER'):
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
@@ -122,8 +126,12 @@ STORAGES = {
     'staticfiles': {'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage'},
 }
 
-# ТИМЛИД: Фикс для автотестов. 
-# Если мы запустили команду test, возвращаем стандартное хранилище статики Django, 
-# которому не нужен манифест файлов.
 if 'test' in sys.argv:
     STORAGES['staticfiles']['BACKEND'] = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+# ТИМЛИД: Настройки безопасности для HTTPS
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
